67f7da56edbedf09cde5e979445d7475b095180c
shpong <79417353+shpong@users.noreply.github.com>
2021-04-13 15:00:12 +0200

    batterie_temperature
    Ajout batterie temperature et batterie a la structure

--- Relais/ESP32_GateWay/BLE_scan.ino
+++ Relais/ESP32_GateWay/BLE_scan.ino
@@ -1,9 +1,10 @@
 // We collect each device caracteristics and store them in BeaconData
+
 typedef struct {
   char id[25] ; //Device Name
   char address[18];   // 67:f1:d2:04:cd:5d
-  //  BLEUUID beaconServiceUUID;
-  //  std::string strManufacturerData;
+  uint8_t batteryLevel;
+  int8_t temperature;
   int rssi;
   int txPower;
 } BeaconData;
@@ -20,40 +21,51 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
       if (bufferIndex >= 50) {
         return;
       }
-
+        uint8_t *payLoad = advertisedDevice.getPayload(); //Get the entire packet (hexdecimal)
+         
       //Name
-      if (advertisedDevice.haveName()) {
-        strcpy (buffer[bufferIndex].id, advertisedDevice.getName().c_str());
-      }
-      //RSSI
-      if (advertisedDevice.haveRSSI()) {
-        buffer[bufferIndex].rssi = advertisedDevice.getRSSI();
-      } else {
-        buffer[bufferIndex].rssi =  0;
-      }
-      //MAC Adresse
-      strcpy (buffer[bufferIndex].address, advertisedDevice.getAddress().toString().c_str());
-
-      //TX Power
-      if (advertisedDevice.haveTXPower()) {
-        buffer[bufferIndex].txPower = advertisedDevice.getTXPower();
-      }
-      //For dev beacons
-      //      if(advertisedDevice.haveServiceUUID())
-      //      {
-      //        buffer[bufferIndex].beaconServiceUUID = advertisedDevice.getServiceUUID();
-      //      }
-      //      if(advertisedDevice.haveManufacturerData() == true)
-      //      {
-      //        buffer[bufferIndex].strManufacturerData = advertisedDevice.getManufacturerData();
-      //      }
-
-      //Debug Print
-      /*Serial.printf("name: %s \n", advertisedDevice.getName().c_str());
-        Serial.printf("MAC: %s \n", advertisedDevice.getAddress().toString().c_str());
-        Serial.printf("Manufactuerer Data: %d \n", advertisedDevice.getManufacturerData());
-        Serial.printf("RSSI: %d \n", advertisedDevice.getRSSI());
-        Serial.printf("TX Power: %d \n", advertisedDevice.getTXPower());*/
+//      if (advertisedDevice.haveName()) {
+//        strcpy (buffer[bufferIndex].id, advertisedDevice.getName().c_str());
+//      }
+//      //MAC Adresse
+//      strcpy (buffer[bufferIndex].address, advertisedDevice.getAddress().toString().c_str());
+//      
+//      //RSSI
+//      if (advertisedDevice.haveRSSI()) {
+//        buffer[bufferIndex].rssi = advertisedDevice.getRSSI();
+//      } else {
+//        buffer[bufferIndex].rssi =  0;
+//      }
+//      
+//
+//      //TX Power
+//      if (advertisedDevice.haveTXPower()) {
+//        buffer[bufferIndex].txPower = advertisedDevice.getTXPower();
+//      }
+//      //For dev beacons
+//      if(advertisedDevice.haveServiceUUID())
+//      {
+//        buffer[bufferIndex].beaconServiceUUID = advertisedDevice.getServiceUUID();
+//        buffer[bufferIndex].nbServiceDataUUID = advertisedDevice.getServiceUUIDCount();
+//        //buffer[bufferIndex].serviceDataUUID = advertisedDevice.getServiceDataUUID();
+//        //buffer[bufferIndex].serviceDataUUIDIndexe = advertisedDevice.getServiceUUID(0x180F);
+//        
+//      }
+//      if(advertisedDevice.haveManufacturerData() == true)
+//      {
+//        buffer[bufferIndex].strManufacturerData = advertisedDevice.getManufacturerData();
+////    buffer[bufferIndex].nbServiceData = advertisedDevice.getServiceDataCount();
+////      buffer[bufferIndex].serviceData = advertisedDevice.getServiceData();
+//      //buffer[bufferIndex].serviceDataIndexe = advertisedDevice.getServiceData(0);
+//
+//      }
+//      
+//      //Debug Print
+//      /*Serial.printf("name: %s \n", advertisedDevice.getName().c_str());
+//        Serial.printf("MAC: %s \n", advertisedDevice.getAddress().toString().c_str());
+//        Serial.printf("Manufactuerer Data: %d \n", advertisedDevice.getManufacturerData());
+//        Serial.printf("RSSI: %d \n", advertisedDevice.getRSSI());
+//        Serial.printf("TX Power: %d \n", advertisedDevice.getTXPower());*/
       bufferIndex++;
     }
 };
@@ -69,13 +81,15 @@ void ScanBeacons() {
 
 
   //Deprecated but kept for dev
-  char whiteList[5][18] = // 10 is the length of the longest string + 1 ( for the '\0' at the end ) DEPRECATED
+  char whiteList[2][18] = // 10 is the length of the longest string + 1 ( for the '\0' at the end ) DEPRECATED
   {
-    "cf:ae:ce:64:a0:f6",
-    "d1:0b:14:b3:18:6a",
-    "fc:02:a0:fa:33:19",
-    "e3:6f:28:36:5a:db",
-    "f1:96:cd:ee:25:bd",
+    "cf:18:14:5d:8a:04",
+    "e2:51:e0:31:ee:0e",
+//    "cf:ae:ce:64:a0:f6",
+//    "d1:0b:14:b3:18:6a",
+//    "fc:02:a0:fa:33:19",
+//    "e3:6f:28:36:5a:db",
+//    "f1:96:cd:ee:25:bd",
   };
 
   //checking whitelist
@@ -87,8 +101,11 @@ void ScanBeacons() {
         strcpy(buffer[nb_detected].address, buffer[i].address);
         buffer[nb_detected].rssi = buffer[i].rssi;
         buffer[nb_detected].txPower = buffer[i].txPower;
-        //        buffer[nb_detected].beaconServiceUUID = buffer[i].beaconServiceUUID;
-        //        buffer[nb_detected].strManufacturerData = buffer[i].strManufacturerData;
+        buffer[nb_detected].beaconServiceUUID = buffer[i].beaconServiceUUID;
+        buffer[nb_detected].strManufacturerData = buffer[i].strManufacturerData;
+        buffer[nb_detected].nbServiceDataUUID = buffer[i].nbServiceDataUUID;
+        buffer[nb_detected].serviceDataUUID = buffer[i].serviceDataUUID;
+        buffer[nb_detected].serviceDataUUIDIndexe = buffer[i].serviceDataUUIDIndexe;
         nb_detected++;
       }
     }
@@ -110,17 +127,32 @@ void ScanBeacons() {
     Serial.print("TX Power: ");
     Serial.println(buffer[i].txPower);
     //For dev beacons
-    //    Serial.print("Service UUID: ");
-    //    Serial.println(buffer[i].beaconServiceUUID.toString().c_str());
-    //    std::string manufacturedData = buffer[i].strManufacturerData;
-    //    uint8_t cManufacturerData[100];
-    //    manufacturedData.copy((char *)cManufacturerData, manufacturedData.length(), 0);
-    //    Serial.printf("strManufacturerData: %d ", manufacturedData.length());
-    //    for (int j = 0; j < manufacturedData.length(); j++)
-    //    {
-    //      Serial.printf("[%X]", cManufacturerData[j]);
-    //    }
-    //    Serial.printf("\n");
+    Serial.print("Service UUID: ");
+    Serial.println(buffer[i].beaconServiceUUID.toString().c_str());
+    
+    std::string manufacturedData = buffer[i].strManufacturerData;
+    uint8_t cManufacturerData[100];
+    manufacturedData.copy((char *)cManufacturerData, manufacturedData.length(), 0);
+    Serial.printf("strManufacturerData: %d ", manufacturedData.length());
+    for (int j = 0; j < manufacturedData.length(); j++)
+    {
+      Serial.printf("%c",cManufacturerData[j]);
+    }
+    Serial.printf("\n");
+    Serial.print("NB Service Data UUID: ");
+    Serial.println(buffer[i].nbServiceDataUUID);
+    Serial.print("Service DATA UUID: ");
+    Serial.println(buffer[i].serviceDataUUID.toString().c_str());
+    Serial.print("Service Data Indexe: ");
+    Serial.println(buffer[i].serviceDataUUIDIndexe.toString().c_str());
+
+    //Don't work 
+//    Serial.print("NB Service Data: ");
+//    Serial.println(buffer[i].nbServiceData);
+//    Serial.print("Service DATA: ");
+//    Serial.println(buffer[i].serviceData.c_str());
+//    Serial.print("Service Data 0: ");
+//    Serial.println(buffer[i].serviceDataIndexe.c_str());
     Serial.println("---------------------");
   }
   bufferIndex = 0;
