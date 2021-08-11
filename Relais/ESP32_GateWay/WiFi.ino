3aadaa9a79c8199ab1b5a77eb72ae9b0e7dc4584
samuelchassot <samuel.chassot@epfl.ch>
2021-08-11 16:33:35 +0200

    first try with wifi hard coded before mqtt

--- Relais/ESP32_GateWay/WiFi.ino
+++ Relais/ESP32_GateWay/WiFi.ino
@@ -9,13 +9,14 @@ void connect_wifi() {
   while (WiFi.status() != WL_CONNECTED) { //Keeps trying to connect until it's ok
     
     Serial.print(".");
-    WiFi.begin(mqttSSID, mqttPasswordWIFI); //tries to connect with the MQTT received credentials
+    WiFi.begin(hardSSID, hardPassword);
+     //tries to connect with the MQTT received credentials
     ledTurquoiseOn(); //Led turquoise
     delay(500); // wait 500ms
 
     //if still not connected
     if (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(hardSSID, hardPassword); //tries to connect with the default credentials
+      WiFi.begin(mqttSSID, mqttPasswordWIFI); //tries to connect with the default credentials
       ledGreenOn(); // 
       delay(500); // wait 500ms
     }
