91421ddfe8dd5a445730198f25561d3abf6200f0
samuelchassot <samuel.chassot@epfl.ch>
2021-08-17 16:52:59 +0200

    version test - works on wss on mosquitto's server

--- test/test_mqtt_over_wss/MQTT_lib_init_struct.c
+++ test/test_mqtt_over_wss/MQTT_lib_init_struct.c
@@ -4,13 +4,13 @@
 esp_mqtt_client_config_t init_esp_mqtt_client_config(const char* serverUri, const char * username, const char * password, const char* lastWillBuffer, int lastWillBufferLength, const char* cert) {
   const esp_mqtt_client_config_t mqtt_cfg = {
         .uri = serverUri,
-        .buffer_size = MQTT_BUFFER_SIZE_RECEIVE,
-        .username = username,
-        .password = password,
+        //.buffer_size = MQTT_BUFFER_SIZE_RECEIVE,
+        //.username = username,
+        //.password = password,
         .lwt_topic = "will",
         .lwt_msg = lastWillBuffer,
         .lwt_msg_len = lastWillBufferLength,
-        //.cert_pem = (const char *)cert,
+        .cert_pem = (const char *)cert,
     };
   return mqtt_cfg;
 }
