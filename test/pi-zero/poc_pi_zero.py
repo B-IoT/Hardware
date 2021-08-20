cdd84ccff33fb0b1e4d329ce5d1d4580b2ebb217
samuelchassot <samuel.chassot@epfl.ch>
2021-08-20 15:56:24 +0200

    bug

--- test/pi-zero/poc_pi_zero.py
+++ test/pi-zero/poc_pi_zero.py
@@ -95,7 +95,6 @@ class relay:
             payload = []
             for k, v in advertisement_data.service_data.items():
                 payload = v
-            print(f"payload utf-8 = {payload.decode('utf-8')}")
             beacon["temperature"] = 22 # TODO
             beacon["battery"] = 42 # TODO
             beacon["timeSinceLastMove"] = 42 # TODO
